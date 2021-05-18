133567e185e58d7edce0869cb42cf1c9a053a5d5
mai <kanaporn.inc@gmail.com>
2021-05-18 15:55:36 +0700

    take a photo and sent file to server

--- webdetect/static/js/main.js
+++ webdetect/static/js/main.js
@@ -54,48 +54,65 @@ $(document).ready(function() {
       switchCard(0);
     });
   
-    $('#upload-button').click(function() {
-        console.log("id buttommmm los");
-    //   $('.modal').modal('open');
-        //const input = document.getElementById('image');
-
-        // add event listener
-        // input.addEventListener('change', () => {
-        //     uploadFile(input.files[0]);
-        // });
-        try {
+    // $('#upload-button').click(function() {
+    //     try {
             
-            let photo = document.getElementById("browse-button").files[0];
-            console.log(photo,"=======",typeof(photo))
-            let formData = new FormData();
+    //         let photo = document.getElementById("browse-button").files[0];
+    //         console.log(photo,"=======",typeof(photo))
+    //         let formData = new FormData();
+    //         console.log(photo.getImageData,"getdata")
+    //         formData.append("image", photo);
+    //         console.log("dta",formData)
+    //         fetch('http://127.0.0.1:5000/api/working/image-detect', {
+    //         //fetch('http://riorocker97.com/api/detect', {
+    //             method: "POST", 
+    //             body: formData
+    //         }).then(response => response.json())
+    //         .then(json => {
+    //           console.log("5555555555 respom",json.image);
+              
+    //          // loadStats(json.image)
+    //           let img64 = "data:image/png;base64, "+json.image;
+              
+    //           var newChild = '<img src="data:image/png;base64, ' + json.image+'"'+ '/>';
+    //           // document.getElementById("detect-result").body.innerHTML = document.write(newChild); 
+    //           document.getElementById("detect-result").innerHTML = newChild; 
+              
+    //         });
             
-            formData.append("image", photo);
-            console.log("dta",formData)
-            //fetch('http://127.0.0.1:5000/api/working/image-detect', {
-            fetch('http://riorocker97.com/api/detect', {
-                method: "POST", 
-                body: formData
-            }).then(response => response.json())
-            .then(json => {
-              console.log("5555555555 respom",json.image);
-              
-             // loadStats(json.image)
-              let img64 = "data:image/png;base64, "+json.image;
-              
-              var newChild = '<img src="data:image/png;base64, ' + json.image+'"'+ '/>';
-              // document.getElementById("detect-result").body.innerHTML = document.write(newChild); 
-              document.getElementById("detect-result").innerHTML = newChild; 
-              
-            });
-            
-           }
-          catch(err) {
-            err.message;
-            alert("errorrr")
-          }
+    //        }
+    //       catch(err) {
+    //         err.message;
+    //         alert("errorrr")
+    //       }
         
-        });
-  });
+    //     });
+    //   });
+
+      // $('#snap').click(function() {
+      //   try{
+      //       img = context.drawImage(video, 0, 0, 320, 240);
+      //       var imageData=context.getImageData(0,0,canvas.width,canvas.height);
+      //       var data=imageData.data;
+      //       console.log("data of canvas",data,typeof(data))
+
+      //       let formData = new FormData();
+            
+      //       formData.append("image", imageData);
+      //       console.log("dta",formData)
+      //       fetch('http://127.0.0.1:5000/api/working/image-detect', {
+      //       //fetch('http://riorocker97.com/api/detect', {
+      //           method: "POST", 
+      //           body: formData
+      //       }).then(response => response.json())
+              
+      //   }
+      //   catch (err) {
+      //     alert("something wrong")
+      //   }
+            
+           
+      // });
   
   switchCard = function(cardNo) {
     var containers = [".dd-container", ".uf-container", ".dt-container"];
